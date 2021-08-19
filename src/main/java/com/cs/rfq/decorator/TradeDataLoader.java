8fdbc06aa2c5a83fb28a6546d7932cabd5ff8d10
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-08-19 13:59:03 -0400

    C2T3 - 23: Trade loader and test fixed, all passing

--- src/main/java/com/cs/rfq/decorator/TradeDataLoader.java
+++ src/main/java/com/cs/rfq/decorator/TradeDataLoader.java
@@ -9,6 +9,7 @@ import org.apache.spark.sql.types.CharType;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+import org.apache.spark.streaming.Time;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,20 +26,20 @@ public class TradeDataLoader {
         //TODO: create an explicit schema for the trade data in the JSON files
         StructType schema =
                 new StructType(new StructField[] {
-                        new StructField("TraderId", IntegerType, false, Metadata.empty()),
-                        new StructField("EntityId", IntegerType, false, Metadata.empty()),
+                        new StructField("TraderId", LongType, false, Metadata.empty()),
+                        new StructField("EntityId", LongType, false, Metadata.empty()),
                         new StructField("MsgType", IntegerType, false, Metadata.empty()),
-                        new StructField("TradeReportId", IntegerType, false, Metadata.empty()),
+                        new StructField("TradeReportId", LongType, false, Metadata.empty()),
                         new StructField("PreviouslyReported", StringType, false, Metadata.empty()),
-                        new StructField("SecurityId", StringType, false, Metadata.empty()),
+                        new StructField("SecurityID", StringType, false, Metadata.empty()),
                         new StructField("SecurityIdSource", IntegerType, false, Metadata.empty()),
-                        new StructField("LastQty", IntegerType, false, Metadata.empty()),
+                        new StructField("LastQty", LongType, false, Metadata.empty()),
                         new StructField("LastPx", DoubleType, false, Metadata.empty()),
                         new StructField("TradeDate", DateType, false, Metadata.empty()),
-                        new StructField("TransactTime", TimestampType, false, Metadata.empty()),
+                        new StructField("TransactTime", StringType, false, Metadata.empty()),
                         new StructField("NoSides", IntegerType, false, Metadata.empty()),
                         new StructField("Side", IntegerType, false, Metadata.empty()),
-                        new StructField("OrderId", StringType, false, Metadata.empty()),
+                        new StructField("OrderId", LongType, false, Metadata.empty()),
                         new StructField("Currency", StringType, false, Metadata.empty())
                 });;
 
