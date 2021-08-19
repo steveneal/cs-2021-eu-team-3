ae593748cfd28d8ac5129ed0ce6708daaba310e0
Neelesh99 <neelesh.ravichandran1999@gmail.com>
2021-08-19 12:04:54 -0400

    C2T3 - 8: TotalTradesWithEntity amended to reflect task and Instrument specific trades extractor added.

--- src/main/java/com/cs/rfq/decorator/RfqProcessor.java
+++ src/main/java/com/cs/rfq/decorator/RfqProcessor.java
@@ -2,16 +2,14 @@ package com.cs.rfq.decorator;
 
 import com.cs.rfq.decorator.extractors.RfqMetadataExtractor;
 import com.cs.rfq.decorator.extractors.RfqMetadataFieldNames;
-import com.cs.rfq.decorator.extractors.TotalTradesWithEntityExtractor;
+import com.cs.rfq.decorator.extractors.TotalTradesWithEntityAndInstrumentExtractor;
 import com.cs.rfq.decorator.extractors.VolumeTradedWithEntityYTDExtractor;
 import com.cs.rfq.decorator.publishers.MetadataJsonLogPublisher;
 import com.cs.rfq.decorator.publishers.MetadataPublisher;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,7 +41,7 @@ public class RfqProcessor {
         //TODO: use the TradeDataLoader to load the trade data archives
 
         //TODO: take a close look at how these two extractors are implemented
-        extractors.add(new TotalTradesWithEntityExtractor());
+        extractors.add(new TotalTradesWithEntityAndInstrumentExtractor());
         extractors.add(new VolumeTradedWithEntityYTDExtractor());
     }
 
--- src/main/java/com/cs/rfq/decorator/extractors/TotalTradesWithEntityAndInstrumentExtractor.java
+++ src/main/java/com/cs/rfq/decorator/extractors/TotalTradesWithEntityAndInstrumentExtractor.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 import static com.cs.rfq.decorator.extractors.RfqMetadataFieldNames.*;
 
-public class TotalTradesWithEntityExtractor implements RfqMetadataExtractor {
+public class TotalTradesWithEntityAndInstrumentExtractor implements RfqMetadataExtractor {
 
     @Override
     public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
@@ -21,6 +21,7 @@ public class TotalTradesWithEntityExtractor implements RfqMetadataExtractor {
         long pastYearMs = DateTime.now().withMillis(todayMs).minusYears(1).getMillis();
 
         Dataset<Row> filtered = trades
+                .filter(trades.col("SecurityId").equalTo(rfq.getIsin()))
                 .filter(trades.col("EntityId").equalTo(rfq.getEntityId()));
 
         long tradesToday = filtered.filter(trades.col("TradeDate").$greater(new java.sql.Date(todayMs))).count();
